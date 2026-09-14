900d07afe427fde56712bbbc04d71734413b8b14
Gaurav Saha <gauravsaha024@gmail.com>
2026-09-14 15:34:28 +0530

    Assignment1

---
+++ Assignment_01_01.py
@@ -0,0 +1,7 @@
+import numpy as np
+marks = np.array([32, 65, 95, 85, 74, 64, 82, 45, 59,97])
+print("Mean:",np.mean(marks))
+print("Median:",np.median(marks))
+print("Standard Deviation:",np.std(marks))
+print("Maximum:",np.max(marks))
+print("Minimum:",np.min(marks))
